ca825db0428741828c6623f41aa29d1ea95c7afe
Flávio Santos <flavio.gds@gmail.com>
2021-05-07 00:50:17 -0300

    update crossOrigin

--- src/main/java/room/meeting/managerapi/controller/MeetingController.java
+++ src/main/java/room/meeting/managerapi/controller/MeetingController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://flaviogds.github.io/meeting_angular")
 @RequestMapping("/api/v1/meeting")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MeetingController {
